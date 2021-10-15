d26813d327d129214de89b80a2695eeb5db24224
Alexjander Bacalso <janderbacalso@gmail.com>
2021-10-15 22:06:02 +0800

    chore: add example
    Signed-off-by: Alexjander Bacalso <janderbacalso@gmail.com>

---
+++ example/src/App.js
@@ -0,0 +1,37 @@
+import ReactJQCloud from 'react-jqcloud';
+import './App.css';
+
+const word_list = [
+  { text: "Lorem", weight: 3 },
+  { text: "Ipsum", weight: 1.5 },
+  { text: "Dolor", weight: 9.4 },
+  { text: "Sit", weight: 8 },
+  { text: "Amet", weight: 6.2 },
+  { text: "Consectetur", weight: 25 },
+  { text: "Techonology", weight: 75 },
+  { text: "Covid", weight: 35 },
+  { text: "Hacktoberfest", weight: 5 },
+  { text: "2021", weight: 5.3 },
+  { text: "Election", weight: 5 },
+  { text: "Philippines", weight: 2.37 },
+  { text: "Typhoon", weight: 3.89 },
+  { text: "Infected", weight: 90 },
+  { text: "Recovery", weight: 20 },
+  { text: "Vaccine", weight: 24 },
+  { text: "Pandemic", weight: 6 }
+];
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        JQCloud Demo
+      </header>
+      <div className="App-body">
+        <ReactJQCloud word_array={word_list}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
